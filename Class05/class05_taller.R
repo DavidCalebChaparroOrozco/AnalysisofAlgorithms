0ac858fffe738b7e1ce323232aef83a8b0bc8d3d
DavidCalebChaparroOrozco <davidcaleb1998@hotmail.com>
2023-03-15 22:45:43 -0500

    Class05: Taller

--- Class05/class05_taller.R
+++ Class05/class05_taller.R
@@ -1,34 +1,28 @@
 library(readxl)
-Regresion_lineal_dolar <- read_excel("C:/Users/salas/Desktop/Class02/AnalysisofAlgorithms/Class05/Regresion_lineal_dolar.xlsx", 
-                                     col_types = c("numeric", "numeric"))
+Regresion_lineal_dolar <- read_excel("Class05/Regresion_lineal_dolar.xlsx", col_types = c("numeric", "numeric"))
 View(Regresion_lineal_dolar)
 
 # Grafique un diagrama de dispersión de los datos.
 # Interpretar.
 
-Regresion_lineal_dolar
-data_x <- Regresion_lineal_dolar$`Fecha (dd/mm/aaaa)`
-data_y <- Regresion_lineal_dolar$`Tasa de cambio representativa del mercado (TRM)`
+data<-Regresion_lineal_dolar
+data_x <- data$`Fecha (dd/mm/aaaa)`
+data_y <- data$`Tasa de cambio representativa del mercado (TRM)`
 plot(data_x, data_y, xlab = "Date", ylab="TRM", main= "Scatter plot")
 
 # Calcule la ecuación de la recta de regresión, interprete la
 # pendiente de la recta de regresión
 
-regression <- lm(data_y~data_x, data = Regresion_lineal_dolar)
+regression <- lm(data_y~data_x, data = data)
 regression
 summary(regression)
 plot(data_x, data_y, xlab= 'Date', ylab='TRM')
 abline(regression)
-# El objetivo de todo esto es encontrar un modelo lineal que minimice las diferencia
-# entre los valores reales (y) y sus estimaciones respectivas, 
-# el error debe ser lo más pequeño (diferencias entre el valor real del Dataset 
-# y su valor estimado en ese punto).
-# El error es la diferencia entre (y) menos (y) la estimada.
 
 # Estime el precio del dólar utilizando la recta de regresión
 # (para cada valor de x) y halle los residuales.
 estimacion <- data.frame(X = seq(1,366))
-predict_r <- predict(regression, estimacion)
+predict_r <- predict(regression, newdata=estimacion)
 # Otra opción
 # predict_r - data_y
 residual <- resid(regression)
@@ -48,5 +42,14 @@ anova(regression)
 pf(0,1,364)
 
 #  Halle e interprete R^2 y r.
+SCE <- 9838270
+SCE
+STC <- 50383409 + 9838270
+STC
 
+R2 <- 1 - (SCE/STC)
+R2
+
+r <- sqrt(R2)
+r
 #  Menciones las ventajas y las desventajas de la regresión lineal simple.
